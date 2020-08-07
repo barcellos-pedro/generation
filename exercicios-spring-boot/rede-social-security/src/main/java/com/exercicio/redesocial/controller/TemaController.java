e582eab614583f0e9500f28a89e77804a96c0fdb
Pedro Barcellos <pedrobarcellosdosreis>
2020-08-07 15:40:34 -0300

    Add projeto, rede social com security

---
+++ exercicios-spring-boot/rede-social-security/src/main/java/com/exercicio/redesocial/controller/TemaController.java
@@ -0,0 +1,58 @@
+package com.exercicio.redesocial.controller;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.exercicio.redesocial.model.Tema;
+import com.exercicio.redesocial.repository.TemaRepository;
+
+@RestController
+@RequestMapping("/temas")
+@CrossOrigin("*")
+public class TemaController {
+	
+	@Autowired
+	private TemaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Tema>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Tema> GetById(@PathVariable Long id){
+		return repository.findById(id).map(response -> ResponseEntity.ok(response)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/buscar-tema/{descricao}")
+	public ResponseEntity<List<Tema>> GetByCategoria(@PathVariable String descricao){
+		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
+	}
+	
+	@PostMapping("/criar-tema")
+	public ResponseEntity<Tema> Post(@RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+	}
+	
+	@PutMapping("/atualizar-tema")
+	public ResponseEntity<Tema> Put(@RequestBody Tema tema){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
+	}
+	
+	@DeleteMapping("/excluir-tema/{id}")
+	public void Delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+}
---
+++ exercicios-spring-boot/rede-social-security/src/main/java/com/exercicio/redesocial/model/Postagem.java
@@ -0,0 +1,74 @@
+package com.exercicio.redesocial.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_postagem")
+public class Postagem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 1, max = 50)
+	private String descricao;
+	
+	private int curtidas;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public long getCurtidas() {
+		return curtidas;
+	}
+
+	public void setCurtidas(int curtidas) {
+		this.curtidas = curtidas;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
