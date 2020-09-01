740b68d947ec4aa0b9b5b003d0c18c6eed953e42
Pedro Barcellos <pedrobarcellosdosreis>
2020-09-01 10:44:03 -0300

    Implementar GET de Tema e Postagem

--- exercicios-front-end/angular/blogPessoal/blogPessoal/src/app/service/postagem.service.ts
+++ exercicios-front-end/angular/blogPessoal/blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
+  }
+
+  getAllPostagens(){
+    return this.http.get('http://localhost:8080/postagens', this.token);
+  }
+
 }
--- exercicios-front-end/angular/blogPessoal/blogPessoal/src/app/service/tema.service.ts
+++ exercicios-front-end/angular/blogPessoal/blogPessoal/src/app/service/tema.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token'))
+  }
+
+  getAllTemas(){
+    return this.http.get('http://localhost:8080/temas', this.token);
+  }
+
+  getByIdTema(id: number){
+    return this.http.get(`http://localhost:8080/temas/${id}`, this.token);
+  }
+
 }
